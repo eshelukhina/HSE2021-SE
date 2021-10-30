9641739ab18cbcd6ebd9b524c6c1f38b30ffffad
Daniel <danielgabitov@>
2021-10-30 22:33:07 +0300

    add error?

--- src/main/java/ru/hse/application/Main.java
+++ src/main/java/ru/hse/application/Main.java
@@ -2,7 +2,7 @@ package ru.hse.application;
 
 public class Main {
     public static void main(String[] args) {
-        Integer snake_case_variable;
+        Integer snake_case_variable, another_variable;
         System.out.println("Dead World!");
     }
 }
