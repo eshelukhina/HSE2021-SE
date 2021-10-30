d609a097ed30fc9bbcd6cfc706b3742cd492b773
Daniel <danielgabitov@>
2021-10-30 22:30:19 +0300

    snake case

--- src/main/java/ru/hse/application/Main.java
+++ src/main/java/ru/hse/application/Main.java
@@ -2,6 +2,7 @@ package ru.hse.application;
 
 public class Main {
     public static void main(String[] args) {
+        Integer snake_case_variable;
         System.out.println("Dead World!");
     }
 }
