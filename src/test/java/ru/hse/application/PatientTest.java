e9291ebeaf102f52ec6cdb1c42b4d9015710d9cb
achaikova <57727520+achaikova@users.noreply.github.com>
2021-11-04 21:22:08 +0300

    fix test

--- src/test/java/ru/hse/application/PatientTest.java
+++ src/test/java/ru/hse/application/PatientTest.java
@@ -3,9 +3,7 @@ package ru.hse.application;
 import jdk.jshell.spi.ExecutionControl.NotImplementedException;
 import org.junit.jupiter.api.Test;
 import ru.hse.application.models.Patient;
-import ru.hse.application.models.Hospital;
-import ru.hse.application.services.DoctorService;
-import ru.hse.application.services.HospitalService;
+import ru.hse.application.services.PatientService;
 
 import java.sql.Timestamp;
 import java.util.List;
@@ -13,13 +11,9 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PatientTest {
-    public void clear() throws NotImplementedException {
-        throw new NotImplementedException("");
-    }
 
     @Test
     public void testBasicAddPatient() throws NotImplementedException {
-        clear();
         String name = "Mark";
         String surname = "Vazowski";
         Timestamp birthday = Timestamp.valueOf("1974-04-13 00:00:00");
